8e79304ab9c63c0cf9ba6af3a7d396633e0c5779
tahir.raza <smtrz@yahoo.com>
2021-03-27 12:45:15 +0500

    New Development: =============== - Revamped complete design of user chosing the way to voyage.

--- app/src/main/java/com/highbryds/fitfinder/ui/Profile/ProfileSettingsFragment.kt
+++ app/src/main/java/com/highbryds/fitfinder/ui/Profile/ProfileSettingsFragment.kt
@@ -1,9 +1,6 @@
 package com.highbryds.fitfinder.ui.Profile
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.preference.PreferenceFragmentCompat
 import com.highbryds.fitfinder.R
 
@@ -11,6 +8,8 @@ class ProfileSettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
+
+
 
 
 }
